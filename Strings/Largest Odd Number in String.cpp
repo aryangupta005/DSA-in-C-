37b3d10ad865156d424c664481978864250e332f
Aryan Gupta <aryangupta005@gmail.com>
2024-07-25 01:56:18 +0530

    string-2

---
+++ Strings/Largest Odd Number in String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string ans="";
+        for(int i=0;i<num.size();i++){
+            if(!nums[i]%2==0){
+            
+            }
+        }
+    }
+};
